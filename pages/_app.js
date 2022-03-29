f0b97c419595428769cfa0dc74ef549b224ed3ec
Robert <u1501851@rts-365.dk>
2022-03-29 15:43:19 +0200

    AuthProvider added to _app

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,14 @@ import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
+import { AuthProvider } from '../contexts/AuthContext'
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />
+	return (
+		<AuthProvider>
+			<Component {...pageProps} />
+		</AuthProvider>
+	)
 }
 
 export default MyApp
