0bcaf277062394234b166143ffc134a63b31efbd
Robert <u1501851@rts-365.dk>
2022-03-30 18:53:45 +0200

    Removed NavBar from _app

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 import { AuthProvider } from '../contexts/AuthContext'
-import NavBar from '../components/NavBar'
 
 function MyApp({ Component, pageProps }) {
 	return (
