a9c918136c2b051b399dd10771254a9fb656895a
Robert <u1501851@rts-365.dk>
2022-04-01 22:09:43 +0200

    Added basic 'create new blog form' to Blogs

--- collections/Blogs.js
+++ collections/Blogs.js
@@ -7,17 +7,38 @@ import {
 	ListItemAvatar,
 	Avatar,
 	ListItemText,
-	Divider,
 	Alert,
 	CircularProgress,
+	ListItemButton,
+	TextField,
 } from '@mui/material'
 import { Delete } from '@mui/icons-material'
 import useFirestore from '../hooks/useFirestore'
+import { useRouter } from 'next/router'
+import { Box } from '@mui/system'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+import LoadingButton from '@mui/lab/LoadingButton'
+import { Save } from '@mui/icons-material'
+
+const blogSchema = yup.object({
+	title: yup.string().required(),
+})
 
 const Blogs = ({ collectionInfo }) => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		clearErrors,
+	} = useForm({
+		resolver: yupResolver(blogSchema),
+	})
 	const [blogs, setBlogs] = useState()
 	const [isLoading, setIsLoading] = useState(true)
 	const [errorMessage, setErrorMessage] = useState()
+	const router = useRouter()
 	const { getCollection } = useFirestore()
 
 	// Get all blogs
@@ -39,24 +60,83 @@ const Blogs = ({ collectionInfo }) => {
 		})()
 	}, [blogs])
 
+	useEffect(() => {
+		console.log(router)
+	}, [router])
+
+	const onSubmit = async (form) => {
+		try {
+			// setIsLoading(true)
+			console.log(form)
+		} catch (error) {
+			setIsLoading(false)
+			setErrorMessage('Something went wrong')
+		}
+	}
+
+	const handleTextFieldOnChange = () => {
+		clearErrors()
+		setErrorMessage(null)
+	}
+
 	return (
 		<>
-			<CollectionHeader collectionName={collectionInfo.collectionName} />
-			<List>
-				{blogs?.map((blog, i) => (
-					<ListItem
-						key={blog.id}
-						secondaryAction={<IconButton edge='end'>{<Delete />}</IconButton>}
-						divider
-						sx={{ py: 2 }}
+			<CollectionHeader
+				collectionName={collectionInfo.collectionName}
+				onClickAddNew={() => router.push(`${router.asPath}&addNew=true`)}
+				onClickSave={handleSubmit(onSubmit)}
+				isLoading={isLoading}
+			/>
+			{/* FORM TO ADD NEW BLOG */}
+			{router.query.addNew && (
+				<Box
+					component='form'
+					// onSubmit={handleSubmit(onSubmit)}
+					sx={{ display: 'grid', gap: '1.5rem' }}
+				>
+					<TextField
+						label='Title *'
+						variant='standard'
+						fullWidth
+						{...register('title')}
+						error={errors?.title ? true : false}
+						helperText={errors?.title && errors.title?.message}
+						onChange={handleTextFieldOnChange}
+					/>
+					<LoadingButton
+						loading={isLoading}
+						onClick={handleSubmit(onSubmit)}
+						variant={'contained'}
+						size={'medium'}
+						startIcon={<Save />}
+						sx={{ justifySelf: 'end', mt: 1 }}
 					>
-						<ListItemAvatar>
-							<Avatar>{collectionInfo.icon}</Avatar>
-						</ListItemAvatar>
-						<ListItemText primary={blog.title} />
-					</ListItem>
-				))}
-			</List>
+						Save
+					</LoadingButton>
+				</Box>
+			)}
+			{/* LIST OF ALL BLOGS */}
+			{!router.query.addNew && (
+				<List>
+					{blogs?.map((blog, i) => (
+						<ListItem
+							key={blog.id}
+							secondaryAction={
+								<IconButton edge='end'>{<Delete />}</IconButton>
+							}
+							divider
+							disablePadding
+						>
+							<ListItemButton sx={{ py: 2 }}>
+								<ListItemAvatar>
+									<Avatar>{collectionInfo.icon}</Avatar>
+								</ListItemAvatar>
+								<ListItemText primary={blog.title} />
+							</ListItemButton>
+						</ListItem>
+					))}
+				</List>
+			)}
 			{errorMessage && <Alert severity='error'>{errorMessage}</Alert>}
 			{isLoading && (
 				<CircularProgress
