95defbf6322fe335a12faced2d6c594697fa5377
Robert <u1501851@rts-365.dk>
2022-04-01 22:08:56 +0200

    Added cancel and save buttons to CollectionHeader

--- components/admin/CollectionHeader.js
+++ components/admin/CollectionHeader.js
@@ -1,7 +1,11 @@
 import { Add } from '@mui/icons-material'
 import { Box, Typography, Button } from '@mui/material'
+import { useRouter } from 'next/router'
+import LoadingButton from '@mui/lab/LoadingButton'
+import { Save, Cancel } from '@mui/icons-material'
 
-const CollectionHeader = ({ collectionName, onClickBtn }) => {
+const CollectionHeader = ({ collectionName, onClickAddNew, onClickSave, isLoading }) => {
+	const router = useRouter()
 	return (
 		<Box component='header' sx={{ display: 'flex', alignItems: 'center', mb: 4 }}>
 			<Typography
@@ -11,15 +15,40 @@ const CollectionHeader = ({ collectionName, onClickBtn }) => {
 			>
 				{collectionName}
 			</Typography>
-			<Button
-				startIcon={<Add />}
-				variant={'contained'}
-				size={'medium'}
-				sx={{ marginLeft: 'auto' }}
-				onClick={onClickBtn}
-			>
-				Add New
-			</Button>
+			{!router.query.addNew && (
+				<Button
+					startIcon={<Add />}
+					variant={'contained'}
+					size={'medium'}
+					sx={{ marginLeft: 'auto' }}
+					onClick={onClickAddNew}
+				>
+					Add New
+				</Button>
+			)}
+			{router.query.addNew && (
+				<>
+					<Button
+						variant={'outlined'}
+						size={'medium'}
+						color='error'
+						sx={{ marginLeft: 'auto', py: 0.63 }}
+						onClick={() => router.back()}
+					>
+						Cancel
+					</Button>
+					<LoadingButton
+						loading={isLoading}
+						onClick={onClickSave}
+						variant={'contained'}
+						size={'medium'}
+						startIcon={<Save />}
+						sx={{ ml: 2 }}
+					>
+						Save
+					</LoadingButton>
+				</>
+			)}
 		</Box>
 	)
 }
