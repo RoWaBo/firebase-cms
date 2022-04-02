f9b26a4f57e3b88d097f006fa889b1dba6642ad6
Robert <u1501851@rts-365.dk>
2022-04-02 10:43:36 +0200

    addDocWithAutoId created in useFirestore

--- hooks/useFirestore.js
+++ hooks/useFirestore.js
@@ -1,4 +1,13 @@
-import { doc, getDoc, getDocs, setDoc, collection, query } from 'firebase/firestore'
+import {
+	doc,
+	getDoc,
+	getDocs,
+	setDoc,
+	addDoc,
+	collection,
+	query,
+	serverTimestamp,
+} from 'firebase/firestore'
 import { db } from '../firebaseConfig'
 
 const useFirestore = () => {
@@ -8,7 +17,16 @@ const useFirestore = () => {
 		ress.forEach((item) => result.push(item.data()))
 		return result
 	}
-	return { getCollection }
+	const addDocWithAutoId = async (collectionName, body) => {
+		const collectionRef = collection(db, collectionName)
+		const ress = await addDoc(collectionRef, body)
+
+		const newDocRef = doc(db, collectionName, ress.id)
+		const newDocBody = { id: ress.id, serverTimestamp: serverTimestamp() }
+		await setDoc(newDocRef, newDocBody, { merge: true })
+		return ress.id
+	}
+	return { getCollection, addDocWithAutoId }
 }
 
 export default useFirestore
