61585f6b8f03b78dd2107251a89a61a76095c478
Robert <u1501851@rts-365.dk>
2022-04-04 17:31:38 +0200

    getDocument and updateDocument in useFirestore

--- hooks/useFirestore.js
+++ hooks/useFirestore.js
@@ -5,8 +5,7 @@ import {
 	setDoc,
 	addDoc,
 	collection,
-	query,
-	serverTimestamp,
+	Timestamp,
 	deleteDoc,
 } from 'firebase/firestore'
 import { db } from '../firebaseConfig'
@@ -18,19 +17,39 @@ const useFirestore = () => {
 		ress.forEach((item) => result.push(item.data()))
 		return result
 	}
+	const getDocument = async (collectionName, docId) => {
+		const ress = await getDoc(doc(db, collectionName, docId))
+		return ress.data()
+	}
+	const updateDocument = async (collectionName, docId, body) => {
+		const docRef = doc(db, collectionName, docId)
+		const currentDate = new Date()
+		const docBody = {
+			...body,
+			editedDate: currentDate,
+		}
+		const updatedDoc = await setDoc(docRef, docBody, { merge: true })
+		return updatedDoc
+	}
 	const addDocWithAutoId = async (collectionName, body) => {
 		const collectionRef = collection(db, collectionName)
 		const ress = await addDoc(collectionRef, body)
-
+		const currentDate = new Date()
 		const newDocRef = doc(db, collectionName, ress.id)
-		const newDocBody = { id: ress.id, serverTimestamp: serverTimestamp() }
+		const newDocBody = { id: ress.id, creationDate: currentDate }
 		await setDoc(newDocRef, newDocBody, { merge: true })
 		return ress.id
 	}
 	const deleteDocument = async (collectionName, id) => {
 		await deleteDoc(doc(db, collectionName, id))
 	}
-	return { getCollection, addDocWithAutoId, deleteDocument }
+	return {
+		getCollection,
+		addDocWithAutoId,
+		deleteDocument,
+		getDocument,
+		updateDocument,
+	}
 }
 
 export default useFirestore
