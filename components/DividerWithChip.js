22a31133d035169a070c333cb47b8357cd3295a9
Robert <u1501851@rts-365.dk>
2022-04-08 16:23:27 +0200

    Created DividerWithChip

---
+++ components/DividerWithChip.js
@@ -0,0 +1,23 @@
+import { Box, Button, Chip, Divider } from '@mui/material'
+
+const DividerWithChip = ({ text, ...props }) => {
+	return (
+		<Box {...props} sx={{ position: 'relative' }}>
+			<Divider />
+			<Chip
+				label={text}
+				size='small'
+				variant='outlined'
+				sx={{
+					backgroundColor: 'white',
+					position: 'absolute',
+					left: '50%',
+					top: '50%',
+					transform: 'translate(-50%, -50%)',
+				}}
+			/>
+		</Box>
+	)
+}
+
+export default DividerWithChip
