00b2d9138cedd9e02394bc0e4c284ecf798e3e4f
Robert <u1501851@rts-365.dk>
2022-04-16 19:45:51 +0200

    display dropped image and handleOnDrop added

--- components/admin/ImageDropzone.js
+++ components/admin/ImageDropzone.js
@@ -4,37 +4,90 @@ import UploadIcon from '@mui/icons-material/Upload'
 import ImageNotSupportedIcon from '@mui/icons-material/ImageNotSupported'
 import { Typography } from '@mui/material'
 import { Box } from '@mui/system'
+import { useState } from 'react'
+import Image from 'next/image'
 
-const ImageDropzone = ({ margin }) => {
+const ImageDropzone = ({ margin, minHeight, setImage, image }) => {
+	const [isLoading, setIsLoading] = useState()
+
+	function ImageUploadIcon({ status }) {
+		if (status.accepted) {
+			return <UploadIcon fontSize={'large'} color='primary' />
+		}
+		if (status.rejected) {
+			return <ImageNotSupportedIcon fontSize={'large'} color='error' />
+		}
+		return (
+			<AddPhotoAlternateIcon fontSize={'large'} sx={{ color: 'text.secondary' }} />
+		)
+	}
+
+	const dropzoneText = (status) => {
+		if (status.accepted) {
+			return 'Drop to upload image'
+		}
+		if (status.rejected) {
+			return 'File is not supported'
+		}
+		return 'Drag image here or click to select file'
+	}
+
 	const dropzoneChildren = (status) => (
-		<Box
-			sx={{
-				width: 'fit-content',
-				display: 'flex',
-				alignItems: 'center',
-				pointerEvents: 'none',
-			}}
-		>
-			{!status.accepted && !status.rejected && (
-				<AddPhotoAlternateIcon fontSize={'large'} />
+		<>
+			{image && (
+				<Box
+					sx={{
+						position: 'relative',
+						width: '100%',
+						height: '150px',
+						pointerEvents: 'none',
+						mb: 1,
+					}}
+				>
+					<Image
+						src={image.url}
+						alt={image.name}
+						layout='fill'
+						quality={85}
+						priority
+						objectFit='contain'
+					/>
+				</Box>
 			)}
-			{status.accepted && <UploadIcon fontSize={'large'} />}
-			{status.rejected && <ImageNotSupportedIcon fontSize={'large'} />}
-			<Typography ml={2}>Drag image here or click to select file</Typography>
-		</Box>
+			<Box
+				sx={{
+					width: '350px',
+					display: 'flex',
+					alignItems: 'center',
+					justifyContent: 'center',
+					pointerEvents: 'none',
+				}}
+			>
+				<ImageUploadIcon status={status} />
+				<Typography ml={2} color='text.secondary'>
+					{dropzoneText(status)}
+				</Typography>
+			</Box>
+		</>
 	)
+
+	const handleOnDrop = (files) => {
+		const imageURL = URL.createObjectURL(files[0])
+		setImage({ url: imageURL, file: files[0] })
+	}
 
 	return (
 		<Dropzone
-			onDrop={(files) => console.log('accepted files', files)}
-			onReject={(files) => console.log('rejected files', files)}
+			onDrop={(files) => handleOnDrop(files)}
+			onReject={(files) => console.log('rejected file', files)}
 			// maxSize={3 * 1024 ** 2}
 			accept={IMAGE_MIME_TYPE}
+			loading={isLoading}
 			style={{
 				margin: margin,
-				minHeight: '70px',
 				display: 'grid',
 				placeContent: 'center',
+				minHeight: minHeight,
 			}}
 		>
 			{(status) => dropzoneChildren(status)}
